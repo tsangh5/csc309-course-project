7740a370d78680ea3afa92ee47490d877462d022
tsangbe3 <benjamin.tsang@mail.utoronto.ca>
2025-11-30 18:02:37 -0500

    adding frontend url

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 
